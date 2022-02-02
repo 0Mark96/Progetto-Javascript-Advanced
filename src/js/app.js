2d11c134e57a3cf2d295fe64b4a627c5d9c90bdb
0mark96 <verardimarco3@gmail.com>
2022-02-02 18:54:09 +0100

    w

--- src/js/app.js
+++ src/js/app.js
@@ -19,7 +19,7 @@ async function searchBooks(event) {
   let output = '';
   await data.works.forEach(book => {
       		output += `
-          <div class="col-lg-4 all-book-container">
+          <div class="col-lg-4 book-btn-container">
           <div class="all-book-container-2">
               <div class="title">
                 <h1>${book.title}</h1>
@@ -45,35 +45,7 @@ async function searchBooks(event) {
 
 // Search fantasy books
 
-document.getElementById('fantasy').addEventListener('click', searchFantasyBooks);
 
-async function searchFantasyBooks(event) {
-
-  const response = await axios.get(`https://openlibrary.org/subjects/fantasy.json?details=true`)
-  const data = await response.data;
-  let output = '';
-  await data.works.forEach(book => {
-      		output += `
-          <div class="all-book-container">
-          <div class="all-book-container-2">
-              <div class="title">
-                <h1>${book.title}</h1>
-              </div>
-              
-              <div class="author">
-                <p>Authors: ` ;
-                book.authors.forEach(author => {output += `${author.name}. `});
-                output +=`</div>
-           </div>
-          <a id="btn-details" onclick="detailsBook('${book.key}')" href="javascript:void(0)"><button>details</button></a>
-          </div>`;
-        });
-      	
-    document.getElementById('book-list').innerHTML = output;
-    if(output == ''){
-      document.getElementById('book-list').innerHTML = `<h4><b>Sorry! No books founds</b></h4>`;
-    }
-}
 let videoTag = document.getElementById('video')
 if(window.screen.width < 840){
   let source = document.createElement("source")
