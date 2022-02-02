c5cf6bb72ab23cea25625570e7b5103b438e2b7d
0mark96 <verardimarco3@gmail.com>
2022-02-02 13:33:22 +0100

    cc

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = (env, argv) => {
       rules: [
            {test: /\.css$/, use: ['style-loader','css-loader', 'sass-loader']
           },
-          {test: /\.(svg|ico|png|webp|jpg|gif|jpeg|mp4)$/, 
+          {test: /\.(svg|ico|png|webp|jpg|gif|jpeg)$/, 
             type: 'asset/resource'
           },
         {
